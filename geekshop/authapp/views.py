65e89db0817cc73e96dc9c7ef8fd34ed6677b00d
AlekseyKh <76121468+MrAxe112@users.noreply.github.com>
2021-10-28 01:48:15 +0500

    lesson_4

---
+++ geekshop/authapp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserChangeForm
+
+
+def login(request):
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+    context = {
+        'login_form': login_form
+    }
+    return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        register_form = ShopUserRegisterForm
+    context = {
+        'register_form': register_form
+    }
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    if request.method == 'POST':
+        edit_form = ShopUserChangeForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserChangeForm(instance=request.user)
+
+    context = {
+        'edit_form': edit_form
+    }
+
+    return render(request, 'authapp/edit.html', context)
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -17,17 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from mainapp import views as mainapp
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
     path('contact/', mainapp.contact, name='contact'),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 
     path('admin/', admin.site.urls),
 
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
